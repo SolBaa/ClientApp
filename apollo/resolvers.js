50994204a6292c3c4e6a2a58a640b4e93637e85d
Sol Battaglia <solbattaglia@gmail.com>
2020-11-11 19:30:44 -0300

    Autenticacion de usuarios

--- apollo/resolvers.js
+++ apollo/resolvers.js
@@ -1,10 +1,23 @@
 const Usuario = require('../models/User')
 const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config({path :'.env'});
+
+const creatToken=(usuario,secreta, expiresIn)=>{
+    const{id, email, nombre, apellido} = usuario
+
+    return jwt.sign({id,email,nombre,apellido}, secreta, {expiresIn})
+
+
+}
 
 // Resolvers 
 const resolvers = {
     Query:{
-        obtenerCursos :() =>"Algo"   
+        obtenerUsuario : async(_, {token}) =>{
+       const usuarioId = await jwt.verify(token, process.env.SECRET)
+            return usuarioId
+    } 
     },
     Mutation :{
         nuevoUsuario: async (_, {input}) => {
@@ -30,6 +43,27 @@ const resolvers = {
                 console.log(error);
             }
 
+        },
+
+        autenticarUsuario:async (_, {input}) =>{
+            const {email, password} = input
+
+            // REvisar si el usuario ya esta registrado
+            const existeUsuario = await Usuario.findOne({email})
+            if (!existeUsuario){
+                throw new Error('El Usuario no exite')
+            }
+            // Revisar si el password es corrrecto
+            const passwordCorrecto = await bcryptjs.compare(password, existeUsuario.password);
+            if (!passwordCorrecto) {
+                throw new Error('El Password es incorrecto ')
+
+
+            }
+            // Crear el token
+            return{
+                token: creatToken(existeUsuario,process.env.SECRET, '24h' )
+            }
         }
 
     }
--- apollo/schema.js
+++ apollo/schema.js
@@ -12,22 +12,29 @@ const typeDefs = gql`
         creado: String
     }
 
+    type Token {
+        token: String
+    }
+
     input UsuarioInput {
         nombre: String!
         apellido: String!
         email: String!
         password: String!
     }
-
-  
+    input AutenticarInput {
+        email: String!
+        password: String!
+    }
 
     type Query{
-        obtenerCursos: String
+        obtenerUsuario(token: String): Usuario
        
     }
 
     type Mutation {
         nuevoUsuario(input: UsuarioInput) : Usuario
+        autenticarUsuario(input: AutenticarInput) : Token
     }
 `
 
